e0cab7a4b74fa2030c3c0ce04aef9bf40098dcf0
gontugithub <gs.alvarez8@gmail.com>
2024-04-24 13:19:22 +0200

    bd_banco modificada. Procedure de modificar cuenta corregida

--- 3aEv/ActividadBanco/bd_banco.sql
+++ 3aEv/ActividadBanco/bd_banco.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-04-2024 a las 12:42:05
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.2
+-- Tiempo de generación: 24-04-2024 a las 13:16:41
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -26,41 +25,40 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_borrar` (IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_borrar` (IN `_id` INT)   begin
 	DELETE FROM cuentas WHERE c_num_cta = _id;
 end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_insertar` (IN `_nif` VARCHAR(9), IN `_titular` VARCHAR(50))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_insertar` (IN `_nif` VARCHAR(9), IN `_titular` VARCHAR(50))   begin
     INSERT INTO cuentas VALUES
 	(null, _nif, _titular, current_date(), 0);
     SELECT last_insert_id() AS id_insertado;
     end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_modificar` (IN `_nif` VARCHAR(9), IN `_titular` VARCHAR(50), IN `_fecha` DATE, IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_modificar` (IN `_nif` VARCHAR(9), IN `_titular` VARCHAR(50), IN `_id` INT)   begin
 		UPDATE cuentas SET
         c_nif = _nif,
-        c_titular = _titular,
-        c_fecha_creacion = _fecha
+        c_titular = _titular
         WHERE 
         c_num_cta = _id;
     end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_por_id` (IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_por_id` (IN `_id` INT)   begin
 		SELECT * FROM cuentas WHERE c_num_cta = _id
         order by c_titular;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_saldo_mayor` (IN `_cantidad` DECIMAL(10,2))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_saldo_mayor` (IN `_cantidad` DECIMAL(10,2))   begin
 		SELECT * FROM cuentas WHERE c_saldo > _cantidad
         order by c_titular;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_saldo_menor` (IN `_cantidad` DECIMAL(10,2))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_saldo_menor` (IN `_cantidad` DECIMAL(10,2))   begin
 		SELECT * FROM cuentas WHERE c_saldo < _cantidad
         order by c_titular;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_todas_filtrado` (IN `_filtro` VARCHAR(50))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_todas_filtrado` (IN `_filtro` VARCHAR(50))   begin
 		SELECT * FROM cuentas WHERE 
 		(c_num_cta LIKE concat('%',_filtro,'%')) OR 
         (c_nif LIKE concat('%',_filtro,'%')) OR 
@@ -69,22 +67,22 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `cta_todas_filtrado` (IN `_filtro` V
         order by c_titular;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_borrar` (IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_borrar` (IN `_id` INT)   begin
 	DELETE FROM movimientos WHERE m_id = _id;
 end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_insertar` (IN `_idCuenta` INT, IN `_importe` DECIMAL(10,2), IN `_concepto` VARCHAR(50))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_insertar` (IN `_idCuenta` INT, IN `_importe` DECIMAL(10,2), IN `_concepto` VARCHAR(50))   begin
     INSERT INTO movimientos VALUES
 	(null, _idCuenta, current_date(),_importe ,_concepto );
     SELECT last_insert_id() AS id_insertado;
     end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_mayor` (IN `_cantidad` DECIMAL(10,2))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_mayor` (IN `_cantidad` DECIMAL(10,2))   begin
 		SELECT * FROM movimientos WHERE m_importe > _cantidad
         order by m_fecha;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_modificar` (IN `_importe` DECIMAL(10,2), IN `_concepto` VARCHAR(50), IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_modificar` (IN `_importe` DECIMAL(10,2), IN `_concepto` VARCHAR(50), IN `_id` INT)   begin
 		UPDATE movimientos SET
         m_importe = _importe,
         m_concepto = _concepto
@@ -92,14 +90,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `m_modificar` (IN `_importe` DECIMAL
         m_id = _id;
     end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_mvtos_entre_fechas` (IN `_cuenta` INT, IN `_desde` DATETIME, IN `_hasta` DATETIME)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_mvtos_entre_fechas` (IN `_cuenta` INT, IN `_desde` DATETIME, IN `_hasta` DATETIME)   begin
 	SELECT * FROM movimientos 
     WHERE
     m_c_num_cta = _cuenta AND
     m_fecha >= _desde AND m_fecha < _hasta;
 end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_todos_filtrado` (IN `_filtro` VARCHAR(50))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_todos_filtrado` (IN `_filtro` VARCHAR(50))   begin
 		SELECT * FROM movimientos WHERE
         (m_id LIKE concat('%',_filtro,'%')) OR
 		(m_c_num_cta LIKE concat('%',_filtro,'%')) OR 
@@ -109,7 +107,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `m_todos_filtrado` (IN `_filtro` VAR
         order by m_fecha;
 	end$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `m_todos_por_id` (IN `_id` INT)  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `m_todos_por_id` (IN `_id` INT)   begin
 		SELECT * FROM movimientos WHERE m_id = _id
         order by m_fecha;
 	end$$
@@ -128,15 +126,18 @@ CREATE TABLE `cuentas` (
   `c_titular` varchar(50) NOT NULL,
   `c_fecha_creacion` date NOT NULL DEFAULT current_timestamp(),
   `c_saldo` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `cuentas`
 --
 
 INSERT INTO `cuentas` (`c_num_cta`, `c_nif`, `c_titular`, `c_fecha_creacion`, `c_saldo`) VALUES
-(0000000003, '05455317Q', 'David Pires Manzanares', '2024-04-23', '1500.00'),
-(0000000004, '33529487A', 'Alex Álvarez de Sotomayor', '2024-04-23', '0.00');
+(0000000003, '05455317Q', 'David Pires Manzanares', '2024-04-23', 1600.00),
+(0000000004, '33529487A', 'Alex Álvarez de Sotomayor', '2024-04-23', 0.00),
+(0000000005, '33529473G', 'Lucía Sánchez', '2024-04-24', 2600.00),
+(0000000006, '98765432J', 'Javier Ruiz Toledo', '2024-04-24', 5000.00),
+(0000000007, '45987436R', 'Gonzalo Sánchez', '2024-04-24', 0.00);
 
 -- --------------------------------------------------------
 
@@ -150,14 +151,18 @@ CREATE TABLE `movimientos` (
   `m_fecha` date NOT NULL DEFAULT current_timestamp(),
   `m_importe` decimal(10,2) DEFAULT NULL,
   `m_concepto` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `movimientos`
 --
 
 INSERT INTO `movimientos` (`m_id`, `m_c_num_cta`, `m_fecha`, `m_importe`, `m_concepto`) VALUES
-(6, 0000000003, '2024-04-23', '1500.00', '\"NÓMINA\"');
+(6, 0000000003, '2024-04-23', 1500.00, '\"NÓMINA\"'),
+(10, 0000000003, '2024-04-24', 200.00, 'PENSION'),
+(11, 0000000005, '2024-04-24', 2600.00, 'NÓMINA ACCENTUR'),
+(12, 0000000003, '2024-04-24', -100.00, '\"GAME\"'),
+(13, 0000000006, '2024-04-24', 5000.00, '\"PENSIÓN JUBILACIÓN\"');
 
 --
 -- Disparadores `movimientos`
@@ -209,13 +214,13 @@ ALTER TABLE `movimientos`
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `c_num_cta` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `c_num_cta` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
-  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
